87df3dfffd92abe34e125e6264308c0e225a6592
Sanjay rajpoot <sanjayrajpoot1165@gmail.com>
2025-02-25 11:28:16 +0530

    [Sanjay] Add EmployeeRepository for data access layer

--- src/main/java/com/example/EmployeePayrollApp/repository/EmployeeRepository.java
+++ src/main/java/com/example/EmployeePayrollApp/repository/EmployeeRepository.java
@@ -41,61 +41,61 @@ public class EmployeeRepository {
     }
 
 
-    public int createEmployee(Employee employee) {
-        String sql = "INSERT INTO employee (name, position, salary) VALUES (?, ?, ?)";
-        return jdbcTemplate.update(sql, employee.getName(), employee.getRole(), employee.getSalary());
-    }
+//    public int createEmployee(Employee employee) {
+//        String sql = "INSERT INTO employee (name, position, salary) VALUES (?, ?, ?)";
+//        return jdbcTemplate.update(sql, employee.getName(), employee.getRole(), employee.getSalary());
+//    }
 
-    public int updateEmployee(Employee employee) {
-        String sql = "UPDATE employee SET name = ?, position = ?, salary = ? WHERE id = ?";
-        return jdbcTemplate.update(sql, employee.getName(), employee.getRole(), employee.getSalary(), employee.getId());
-    }
+//    public int updateEmployee(Employee employee) {
+//        String sql = "UPDATE employee SET name = ?, position = ?, salary = ? WHERE id = ?";
+//        return jdbcTemplate.update(sql, employee.getName(), employee.getRole(), employee.getSalary(), employee.getId());
+//    }
 
-    public int deleteEmployee(Long id) {
-        String sql = "DELETE FROM employee WHERE id = ?";
-        return jdbcTemplate.update(sql, id);
-    }
+//    public int deleteEmployee(Long id) {
+//        String sql = "DELETE FROM employee WHERE id = ?";
+//        return jdbcTemplate.update(sql, id);
+//    }
 
-    public List<Employee> findAll() {
-        String sql = "SELECT * FROM employee";
-        return jdbcTemplate.query(sql, (rs, rowNum) -> {
-            Employee employee = new Employee();
-            employee.setId((int) rs.getLong("id"));
-            employee.setName(rs.getString("name"));
-            employee.setRole(rs.getString("position"));
-            employee.setSalary(rs.getDouble("salary"));
-            return employee;
-        });
-    }
+//    public List<Employee> findAll() {
+//        String sql = "SELECT * FROM employee";
+//        return jdbcTemplate.query(sql, (rs, rowNum) -> {
+//            Employee employee = new Employee();
+//            employee.setId((int) rs.getLong("id"));
+//            employee.setName(rs.getString("name"));
+//            employee.setRole(rs.getString("position"));
+//            employee.setSalary(rs.getDouble("salary"));
+//            return employee;
+//        });
+//    }
 
-    public Optional<Employee> findById(int id) {
-        String sql = "SELECT * FROM employee WHERE id = ?";
-        try {
-            Employee employee = jdbcTemplate.queryForObject(sql, new Object[]{id}, (rs, rowNum) -> {
-                Employee e = new Employee();
-                e.setId((int) rs.getLong("id"));
-                e.setName(rs.getString("name"));
-                e.setRole(rs.getString("position"));
-                e.setSalary(rs.getDouble("salary"));
-                return e;
-            });
-            return Optional.ofNullable(employee);
-        } catch (Exception e) {
-            return Optional.empty();
-        }
-    }
+//    public Optional<Employee> findById(int id) {
+//        String sql = "SELECT * FROM employee WHERE id = ?";
+//        try {
+//            Employee employee = jdbcTemplate.queryForObject(sql, new Object[]{id}, (rs, rowNum) -> {
+//                Employee e = new Employee();
+//                e.setId((int) rs.getLong("id"));
+//                e.setName(rs.getString("name"));
+//                e.setRole(rs.getString("position"));
+//                e.setSalary(rs.getDouble("salary"));
+//                return e;
+//            });
+//            return Optional.ofNullable(employee);
+//        } catch (Exception e) {
+//            return Optional.empty();
+//        }
+//    }
 
 
-    public Employee save(Employee employee) {
-        if (employee.getId() == 0) {
-            // If id is 0, create a new employee
-            createEmployee(employee);
-        } else {
-            // If id is not 0, update the existing employee
-            updateEmployee(employee);
-        }
-        return employee;
-    }
+//    public Employee save(Employee employee) {
+//        if (employee.getId() == 0) {
+//            // If id is 0, create a new employee
+//            createEmployee(employee);
+//        } else {
+//            // If id is not 0, update the existing employee
+//            updateEmployee(employee);
+//        }
+//        return employee;
+//    }
 
     public boolean existsById(int id) {
         String sql = "SELECT COUNT(*) FROM employee WHERE id = ?";
